35d77496ceb4347fad359f1b49c25490b43878a1
steven arellano <stevenjarellano2@gmail.com>
2022-12-22 14:27:11 -0800

    added takeaways

--- ten-takeaways-frontend/context/takeaways.js
+++ ten-takeaways-frontend/context/takeaways.js
@@ -1,6 +1,9 @@
 export const BOOK_INFO = [
     { title: "The Language of Sales", author: "Tom Hopkins" },
-    { title: "The 10x Rule", author: "Grant Cardone" }
+    { title: "The 10x Rule", author: "Grant Cardone" },
+    { title: "Clean Code", author: "Robert Cecil Martin" },
+    { title: "Eloquent Javascript", author: "Marijn Haverbeke" },
+    { title: "Can't Hurt Me", author: "David Goggins" }
 ]
 
 export const TAKEAWAYS = {
@@ -27,5 +30,41 @@ export const TAKEAWAYS = {
         "Rid yourself of average thinking and average action.",
         "Create “only” practices.What is something only you are doing?",
         "Time management is more about knowing your priorities clearly than finding balance."
+    ],
+    "Clean Code": [
+        "Prefer polymorphism to if/else or switch/case statements",
+        "Output and flag arguments are a red flag",
+        "Functions should do one thing. They should do it well. They should do it only.",
+        "Three laws of TDD",
+        "Design Rules: 1. Runs all the tests, 2. No duplication, 3. Expressive, 4. Minimal classes and methods.",
+        "Builds should only take one step",
+        "Good programs are compiled of files that are usually around 200 lines long",
+        "Don’t do train wreck code (ctxt.get().put().show())",
+        "Never return or pass NULL - make assertions in functions against this",
+        "In concurrency, use 1. Single responsibility principle, 2. Corollary: limit the scope of data, 3. Corollary: use copies of data, 4. Corollary: threads should be as independent as possible"
+    ],
+    "Eloquent Javascript": [
+        "Can pass too many or too little arguments",
+        "The difference between var and let and const",
+        "Javascript naming conventions",
+        "How closures work",
+        "Value.1 == value[1]",
+        "Immutable objects are passed as references",
+        "Object shorthand",
+        "Object prototype the main one",
+        "Strict mode",
+        "Error catching"
+    ],
+    "Can't Hurt Me": [
+        "Take souls: It’s about being so incredible at what you do, that it forces other people to respect you out of sheer awe.",
+        "It doesn’t always go your way",
+        "The 40% Rule: When your mind is telling you that you’re done, you’re only 40% away from your maximum potential.",
+        "“No one is going to come help you. No one's coming to save you.”",
+        "Pain is the Way",
+        "“Heraclitus, a philosopher born in the Persian Empire back in the fifth century BC, had it right when he wrote about men on the battlefield. “Out of every one hundred men,” he wrote, “ten shouldn’t even be there, eighty are just targets, nine are the real fighters, and we are lucky to have them, for they make the battle. Ah, but the one, one is a warrior…”",
+        "Progress is motivating: “We all need small sparks, small accomplishments in our lives to fuel the big ones.”",
+        "“In the military we always say we don’t rise to the level of our expectations, we fall to the level of our training,”",
+        "“Everything in life is a mind game! Whenever we get swept under by life’s dramas, large and small, we are forgetting that no matter how bad the pain gets, no matter how harrowing the torture, all bad things end.”",
+        "“Pain unlocks a secret doorway in the mind, one that leads to both peak performance, and beautiful silence.”"
     ]
 }
