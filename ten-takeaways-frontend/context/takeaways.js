4e93234ca6cec0b6329d46f1cbd4116dc75423ab
steven arellano <stevenjarellano2@gmail.com>
2022-12-22 04:50:46 -0800

    2 takeaways

--- ten-takeaways-frontend/context/takeaways.js
+++ ten-takeaways-frontend/context/takeaways.js
@@ -1,18 +1,31 @@
 export const BOOK_INFO = [
-    { title: "Reality is Broken", author: "Jane McGonigal" }
+    { title: "The Language of Sales", author: "Tom Hopkins" },
+    { title: "The 10x Rule", author: "Grant Cardone" }
 ]
 
 export const TAKEAWAYS = {
-    "Reality is Broken": [
-        "Life is cool.",
-        "Life is cool.",
-        "Life is cool.",
-        "Life is cool.",
-        "Life is cool.",
-        "Life is cool.",
-        "Life is cool.",
-        "Life is cool.",
-        "Life is cool.",
-        "Life is cool.",
+    "The Language of Sales": [
+        "7 basics that will make you great: prospecting, making original contact the professional way, qualifications, presentation, handling objections, closing the sale, referrals.",
+        "Question right and sink your teeth into success.",
+        "Tie downs are like cheat codes in questioning.",
+        "Don’t sell logic, arouse emotions.",
+        "Main motivators are money, security, achievement, recognition, acceptance by others, self-acceptance, love of family.",
+        "Main demotivators are fear of losing security, fear of failure, self-doubt, pain of change.",
+        "Where to find people to sell to: Itch cycle, technical advancement, local newspoaper, etc.",
+        "The card-referral system is a nice way to get referrals with the methods above.",
+        "There are different standards for pre planning your presentation in writing.",
+        "Always have your closing materials with you, make sure to test close so that they are sure they want to buy and your will for sure get the close.",
+    ],
+    "The 10x Rule": [
+        "Common mistakes: setting your sights too low, underestimating how much action is required, spending too much time competing and not enough time dominating their sector, and underestimating the amount of adversity they will have to overcome",
+        "Nobody wins when you diminish the importance of success",
+        "Being dependent on only one person or one solution for success is your fault",
+        "Success by others is an indication that something is possible.It should inspire you.",
+        "If you're willing to take credit when you win then you have to be willing to take responsibility when you lose.",
+        "If you were really legit, people would come to you.Stop driving and flying to everyone.Step up your game.",
+        "Money and power follow attention.",
+        "Rid yourself of average thinking and average action.",
+        "Create “only” practices.What is something only you are doing?",
+        "Time management is more about knowing your priorities clearly than finding balance."
     ]
 }
